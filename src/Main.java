56f6042674636e3e51eb6e0f0c9ab4b59bbe4a0f
Zhenishbek <zhenishbekasangaziev@gmail.com>
2022-10-25 13:13:21 +0600

    homework 10

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import org.w3c.dom.css.CSSStyleDeclaration;
+
+import javax.xml.stream.FactoryConfigurationError;
+import java.util.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+myMethod(20,23,32,4,20,42,45);
+        }
+        public static int myMethod(int num ,int...numbers) {
+
+        return myMethod(num,numbers);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
